493cb75d64d8da1f5d2d848af15b9d4e356fa350
Forgot <fxrgxt08@gmail.com>
2025-09-24 11:41:20 -0400

    Add files via upload

---
+++ 5 Year age calculator.py
@@ -0,0 +1,15 @@
+age = 14
+name = "Alex"
+is_student = True
+
+
+print (name, "is" , age, "years old.")
+print (name, "is a student:", is_student)
+
+name = input("What's your name?")
+print("Hello", name)
+age = input("How old are you?")
+print(f"I am {age} years old")
+age = int(age)
+print(name, "is, age years old")
+print("In 5 years, you will be", age + 5)
